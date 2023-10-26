a482cee148857669bfb0b81748e1b033f748f497
CodeNinjasEtobicoke <148917437+iam9116@users.noreply.github.com>
2023-10-26 18:04:20 -0400

    aqwsdefg
    kgftyfggfhgh

---
+++ Assets/Scripts/itemBoxSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxSpawner : MonoBehaviour
+{
+    public GameObject itemBox;
+
+    public int numberOfBoxes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfBoxes; i++)
+        {
+            GameObject itemBoxClone = Instantiate(itemBox, transform.position, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
